1541eb70fa185fdf23855aaf6b3c76e5fc8df084
jmatela26 <jmatela26@gmail.com>
2024-03-11 22:07:37 +0800

    Added fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import React from 'react';
+import { Profile } from './Profile/Profile';
+
+import { Statistics } from './Statistics/Statistics';
+import data from '../data/data.json'; //IT IS THE DOWNLOADED JSON FORM THE TASK
+
+import user from '../data/user.json';
+import avatar from '../images/josiahmatela.jpg';
+
+import { FriendList } from './FriendList/FriendList';
+import friends from '../data/friends.json'; //IT IS THE DOWNLOADED JSON FORM THE TASK
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transaction.json'; //IT IS THE DOWNLOADED JSON FORM THE TASK
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={avatar}
+        stats={user.stats}
+      />
+      <FriendList friends={friends} />
+      <Statistics title="Upload Stats" stats={data} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
